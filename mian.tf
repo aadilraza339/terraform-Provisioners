1616c5d7cdd74de9a9613e58f321e470db2be19b
aadil raza <aadil18@navgurukul.org>
2023-05-12 21:53:33 +0530

    module as a project

--- mian.tf
+++ mian.tf
@@ -1,3 +1,18 @@
-output "rendered_template" {
-    value = templatefile("./backends.tpl", { port = 8080, ip_addrs = ["10.0.0.1", "10.0.0.2"] })
+
+
+provider "aws" {
+    region = "eu-west-1"
+}
+
+module "work_queue" {
+    source = "./sqs-with-backoff"
+    queue_name = "work-queue"
+}
+
+output "work_queue_name" {
+    value = module.work_queue.queue_name
+}
+
+output "work_queue_dead_letter_name" {
+    value = module.work_queue.dead_letter_queue_name
 }
